4fb1561c26cb1ea22a2f3471cef0061d0a822259
BorakL <lborak88@gmail.com>
2024-12-22 18:59:36 +0100

    changed app

--- rhf-demo/src/App.tsx
+++ rhf-demo/src/App.tsx
@@ -3,12 +3,13 @@ import './App.css';
 import { YoutubeForm } from './components/youtubeForm';
 import PlainReactForm from './components/plainReactForm';
 import Form2 from './components/form2';
+import Form3 from './components/form3';
 
 function App() {
   return (
     <div className="App">
       {/* <YoutubeForm/> */}
-      <Form2/>
+      <Form3/>
     </div>
   );
 }
